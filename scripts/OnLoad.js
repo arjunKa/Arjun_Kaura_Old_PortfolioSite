73d5738f155d294537c66e0fab12d0262adf6e8a
arjunKa <32141878+arjunKa@users.noreply.github.com>
2022-02-22 22:28:48 -0500

    Update OnLoad.js

--- scripts/OnLoad.js
+++ scripts/OnLoad.js
@@ -1,10 +1,20 @@
 window.onload = function () {
 
     if (getCookie("style_sheet") != "") {
+        //set style
         document.getElementById("pagestyle").setAttribute("href", getCookie("style_sheet"));
-        if (document.getElementById("pagestyle").getAttribute("href") == "styles/dark-style.css")
+
+        //set text as Light mode:
+        if (document.getElementById("pagestyle").getAttribute("href") == "styles/dark-style.css") {
             document.getElementById("drkMode").textContent = "Light Mode";
+        } else {
+            //set text as Dark mode:
+            document.getElementById("drkMode").textContent = "Dark Mode";
+
+        }
     } else {
+
+        //if no cookies: do light mode
         setCookie("style_sheet", "styles/style.css", 30);
         document.getElementById("drkMode").textContent = "Dark Mode";
     }
