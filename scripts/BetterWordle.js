2287c27e7ee9bf7717a6937793851eb4fcab60d7
arjunKa <32141878+arjunKa@users.noreply.github.com>
2022-04-06 17:19:24 -0400

    many fixes
    fixed wordle styling of page, removed bad words

--- scripts/BetterWordle.js
+++ scripts/BetterWordle.js
@@ -6,20 +6,20 @@ var gameEnd = false;
 
 function getElementByTextContent(text) {
     var spanList = document.getElementsByTagName("button");
-    console.log("find button");
+
     for (var i = 0, len = spanList.length; i < len; i++) {
         if (spanList[i].textContent.toLowerCase() == text.toLowerCase()) // use .innerHTML if you need IE compatibility
             return spanList[i]
     }
 }
-
+var val;
 function keyboardCheck(keyInput) {
 
     if (!gameEnd) {
 
         if (typeof keyInput == "string") {
             keyInput = keyInput.charCodeAt(0);
-            console.log(keyInput);
+
         }
 
         if (/^[a-zA-Z]*$/g.test(String.fromCharCode(keyInput))) {
@@ -43,7 +43,7 @@ function keyboardCheck(keyInput) {
             }
 
         } else if (keyInput == 13) {//enter
-
+            //check_if_word_exists(getFullWord());
             if (indexCol == 4 && arr_of_words.includes(getFullWord())) {
 
                 if (getFullWord() == word) {
@@ -62,6 +62,7 @@ function keyboardCheck(keyInput) {
 
 
             } else {
+                //console.log(check_if_word_exists(getFullWord()));
                 var popup = document.getElementById("myPopup");
                 popup.classList.toggle("show");
 
@@ -70,7 +71,7 @@ function keyboardCheck(keyInput) {
                     (popup.classList.toggle("hide"))
                 }, 1990);
 
-                console.log("Not in word list.");
+
 
             }
 
@@ -120,8 +121,9 @@ var arr_of_words;
 function chooseWord(arr) {
     //setArr(test)
     var index = Math.floor(Math.random() * arr.length);
-    console.log(arr[index]);
+
     word = arr[index];
+
     // arr_of_words = arr;
     setCookie("wordle_answer", + arr[index], 0.1);
 
@@ -133,9 +135,7 @@ function chooseWord(arr) {
         }
     }
 
-    for (let [key, value] of map1) {
-        console.log(key + " = " + value);
-    }
+
 
 }
 
@@ -167,8 +167,7 @@ function checkWord() {
 
         ans_char = word.charAt(i);
 
-        //console.log("userCh: " + your_char);
-        //console.log("ansCh: " + ans_char);
+
 
         if (your_char == ans_char) {
 
@@ -176,8 +175,7 @@ function checkWord() {
                 getElementsByTagName("th")[i].style.backgroundColor = "green";
             map2.set(your_char, map2.get(your_char) - 1);
             getElementByTextContent(your_char).style.backgroundColor = "green";
-            //console.log("ansCh: " + map2.get(your_char));
-            //console.log("ansCh: " + map1.get(your_char));
+
         }
     }
 
@@ -187,14 +185,14 @@ function checkWord() {
         your_char = user_answer.charAt(i);
 
         ans_char = word.charAt(i);
-        console.log(your_char + " map: " + map1.get(your_char));
+
         if (your_char != ans_char && word.includes(your_char) && map2.get(your_char) > 0) {
 
             document.getElementsByTagName("table")[0].getElementsByTagName("tr")[indexRow].
                 getElementsByTagName("th")[i].style.backgroundColor = "DarkOrange";
             getElementByTextContent(your_char).style.backgroundColor = "DarkOrange";
             map2.set(your_char, map2.get(your_char) - 1);
-            console.log(your_char + " map: " + map2.get(your_char));
+
         } else if (!word.includes(your_char)) {
             getElementByTextContent(your_char).style.backgroundColor = "gray"
             document.getElementsByTagName("table")[0].getElementsByTagName("tr")[indexRow].
@@ -202,7 +200,7 @@ function checkWord() {
 
         }
     }
-    //console.log("user: " + user_answer);
+
 
 
 }
@@ -272,4 +270,46 @@ function modalForIntro() {
     }
 
 }
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+const word_data = {};
+function check_if_word_exists(words) {
+
+    fetch(url + words)
+        .then(response => {
+            if (response.ok) {
+                val = true;
+                console.log("ok")
+                return response.json()
+
+            } else if (response.status === 404) {
+                val = false;
+                return Promise.reject('error 404')
+            } else {
+                val = false;
+                return Promise.reject('some other error: ' + response.status)
+            }
+        })
+        .then(data => console.log('data is', data))
+        .then(() => setVal(true))
+        .catch(error => console.log('error is', error));
+
+
+}
+
+function setVal(val2) {
+
+    val = val2
+
+}
+
+window.mobileCheck = function () {
+    let check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+// if (window.mobileCheck()) {
+
+//     document.getElementById("pagestyle").getAttribute("href") == "styles/mobile.css"
+// }
 
